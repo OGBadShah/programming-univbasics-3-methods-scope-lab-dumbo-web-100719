2512b5439d19d96b2d19e8cb3d51e412544754a8
iamnabeel2 <iamnabeel2@gmail.com>
2019-09-24 23:21:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,3 +10,15 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+
+def link
+  phrase "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+end
+
+any_phrase
